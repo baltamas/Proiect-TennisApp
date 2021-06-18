3e6416e80962f02e77de8c1e5c7b279870bab5a5
AnamariaBursuc <anamaria_bursuc@yahoo.com>
2021-06-18 16:21:07 +0300

    Testing Start

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TennisApp.Models;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+          
+        }
+    }
+}
