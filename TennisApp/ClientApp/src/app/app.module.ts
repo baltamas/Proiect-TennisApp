313a228ede13aaaae4d04994db88dae1f259c0cc
Anca Avram <avram.anca.alexandra@gmail.com>
2021-06-23 05:54:14 +0300

    tests Players - add player service

--- TennisApp/ClientApp/src/app/app.module.ts
+++ TennisApp/ClientApp/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { PlayersListComponent } from './players/players-list/players-list.component';
 import { MatchesListComponent } from './matches/matches-list/matches-list.component';
+import { MatchModule } from './match/match.module';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { MatchesListComponent } from './matches/matches-list/matches-list.compon
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
+    MatchModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
---
+++ TennisApp/Services/PlayersService.cs
@@ -0,0 +1,77 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TennisApp.Data;
+using TennisApp.Models;
+using TennisApp.ViewModel;
+namespace TennisApp.Services
+{
+    public class PlayersService
+    {
+        public ApplicationDbContext _context;
+
+        public PlayersService(ApplicationDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public async Task<ServiceResponse<List<Player>, IEnumerable<EntityManagementError>>> GetPlayers()
+        {
+            var players = await _context.Player.ToListAsync();
+            var serviceResponse = new ServiceResponse<List<Player>, IEnumerable<EntityManagementError>>();
+            serviceResponse.ResponseOk = players;
+            return serviceResponse;
+        }
+        public async Task<ServiceResponse<Player, IEnumerable<EntityManagementError>>> GetPlayersById(int id)
+        {
+            var players = await _context.Player.FindAsync(id);
+
+            var serviceResponse = new ServiceResponse<Player, IEnumerable<EntityManagementError>>();
+            serviceResponse.ResponseOk = players;
+            return serviceResponse;
+        }
+        public async Task<ServiceResponse<Player, IEnumerable<EntityManagementError>>> PostPlayer(Player player)
+        {
+            _context.Player.Add(player);
+            var serviceResponse = new ServiceResponse<Player, IEnumerable<EntityManagementError>>();
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                serviceResponse.ResponseOk = player;
+            }
+            catch (Exception e)
+            {
+                var errors = new List<EntityManagementError>();
+                errors.Add(new EntityManagementError { Code = e.GetType().ToString(), Description = e.Message });
+            }
+
+            return serviceResponse;
+        }
+        public async Task<ServiceResponse<bool, IEnumerable<EntityManagementError>>> DeletePlayer(int playerId)
+        {
+            var serviceResponse = new ServiceResponse<bool, IEnumerable<EntityManagementError>>();
+
+            try
+            {
+                var player = await _context.Player.FindAsync(playerId);
+                _context.Player.Remove(player);
+                await _context.SaveChangesAsync();
+                serviceResponse.ResponseOk = true;
+            }
+            catch (Exception e)
+            {
+                var errors = new List<EntityManagementError>();
+                errors.Add(new EntityManagementError { Code = e.GetType().ToString(), Description = e.Message });
+            }
+
+            return serviceResponse;
+        }
+
+    }
+}
